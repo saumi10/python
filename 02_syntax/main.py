0f02b2ab40c7ca649db30df8487e6aa833a056eb
saumi10 <saumi2310@gmail.com>
2025-05-16 02:07:29 +0530

    basics

---
+++ 02_syntax/main.py
@@ -0,0 +1,22 @@
+#how to print
+print("heyy")
+
+#if statement example
+a=10
+if(a>3):
+    print("inside if")
+    # indentation means we are inside the if statement
+
+# we are now outside the if block 
+print("outside if") 
+
+#program to convert lower case texts into upper case
+text=input("Enter name in lowercase:") #get user input
+uppercase_text=text.upper() #convert using built in function
+print("Name in uppercase:",uppercase_text) #print uppercase text
+
+'''
+This is how u write
+a multi line comment 
+using triple single quotes
+'''
